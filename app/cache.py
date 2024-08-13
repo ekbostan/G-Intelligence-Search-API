347e62400a85bb11ba84f5cb33fb4d6c06c6c554
Erol Kaan Bostan <bostanerolkaan@gmail.com>
2024-08-12 23:55:41 -0700

    added memcahce failover

--- app/cache.py
+++ app/cache.py
@@ -1,11 +1,16 @@
 import bmemcached
 import os
 from dotenv import load_dotenv
+import logging
 
 load_dotenv()
-
-memcached_client = bmemcached.Client(
-    f"{os.getenv('MEMCACHED_HOST')}:{os.getenv('MEMCACHED_PORT')}",
-    username=os.getenv('MEMCACHED_USERNAME'),
-    password=os.getenv('MEMCACHED_PASSWORD')
-)
+try:
+    memcached_client = bmemcached.Client(
+        f"{os.getenv('MEMCACHED_HOST')}:{os.getenv('MEMCACHED_PORT')}",
+        username=os.getenv('MEMCACHED_USERNAME'),
+        password=os.getenv('MEMCACHED_PASSWORD')
+    )
+    logging.info("Memcached client initialized successfully.")
+except Exception as e:
+    logging.error(f"Failed to initialize memcached client: {e}")
+    memcached_client = None
